4ce2e391d92718b282e519c1981f6437be801b04
adrien.moliere <adrien.moliere@1482aac9-9606-e74e-8cee-6e872d75cf78>
2011-10-20 21:46:18 +0000

    merde

--- Yello Karto/moteur3D.ml
+++ Yello Karto/moteur3D.ml
@@ -12,7 +12,6 @@ let initGL _ =
 	GlMat.load_identity ();
 	GlClear.clear [`depth ; `color];
 	Gl.enable `depth_test
-   
 
 let display3D _ =
 GlClear.clear [`depth ; `color];
@@ -22,37 +21,47 @@ GlClear.clear [`depth ; `color];
 	
 	GlDraw.color (1., 0., 0.);
 	GlDraw.vertex3 (1., 1., 1.);
+	GlDraw.color (0.263, 0.654, 0.132);
 	GlDraw.vertex3 (1., 1., -1.);
+	GlDraw.color (0.3, 0.8, 0.2);
 	GlDraw.vertex3 (-1., 1., -1.);
+	GlDraw.color (0.154, 0.221, 0.665);
 	GlDraw.vertex3 (-1., 1., 1.);
 	
 	GlDraw.color (0., 1., 0.);
 	GlDraw.vertex3 (1., -1., 1.);
+	GlDraw.color (0.456, 0.853, 0.667);
 	GlDraw.vertex3 (1., -1., -1.);
+	GlDraw.color (0.758, 0.247, 0.467);
 	GlDraw.vertex3 (1., 1., -1.);
+	GlDraw.color (0.587, 0.885, 0.21);
 	GlDraw.vertex3 (1., 1., 1.);
 	
 	GlDraw.color (0., 0., 1.);
 	GlDraw.vertex3 (-1., -1., 1.);
 	GlDraw.vertex3 (-1., -1., -1.);
+	GlDraw.color (0.738, 0.48, 0.83);
 	GlDraw.vertex3 (1., -1., -1.);
 	GlDraw.vertex3 (1., -1., 1.);
 	
 	GlDraw.color (0.8, 0.1, 0.89);
 	GlDraw.vertex3 (-1., 1., -1.);
 	GlDraw.vertex3 (1., 1., -1.);
+	GlDraw.color (0.32, 0.425, 0.6);
 	GlDraw.vertex3 (1., -1., -1.);
 	GlDraw.vertex3 (-1., -1., -1.);
 	
 	GlDraw.color (0.7, 0.5, 0.12);
 	GlDraw.vertex3 (-1., 1., 1.);
 	GlDraw.vertex3 (-1., 1., -1.);
+	GlDraw.color (0.6, 0.1, 0.2);
 	GlDraw.vertex3 (-1., -1., -1.);
 	GlDraw.vertex3 (-1., -1., 1.);
 	
 	GlDraw.color (0.05, 0.60, 1.);
 	GlDraw.vertex3 (-1., 1., 1.);
 	GlDraw.vertex3 (1., 1., 1.);
+	GlDraw.color (0.415, 0.656, 0.32);
 	GlDraw.vertex3 (1., -1., 1.);
 	GlDraw.vertex3 (-1., -1., 1.);	
 	GlDraw.ends ();	
